408dcbd223b92a4248658a3933688fa36d7f8f1c
Ubaidullah-Hasan <hm2964133@gmail.com>
2023-06-09 19:18:55 +0600

    firebase.js file create

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD5iM2epI6gYuyOrOGiV1Xdzu7frZSlMo0",
+    authDomain: "kriya-yoga-c39fd.firebaseapp.com",
+    projectId: "kriya-yoga-c39fd",
+    storageBucket: "kriya-yoga-c39fd.appspot.com",
+    messagingSenderId: "35267694286",
+    appId: "1:35267694286:web:74c84410c0c6787a460a9f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
